dc908e212307d5a96bcbf7052b3433f9d668f42b
SamanthaVP <sam@gmail.com>
2022-09-30 16:59:15 -0500

    My Django Girls app, first commit

---
+++ Samantha/prueba1/suma.py
@@ -0,0 +1,11 @@
+#from django.shortcuts import render
+#from .models import Campo 
+
+#def muestraDatos (request):
+#consulta Campo.objects.all() listasuma calculasuma(consulta)
+#contexto zip(consulta, listasuma)
+#return render(request, 'blog/index.html", ('contexto': contexto})
+#def calculasuma(lista):
+#listaSuma-[] for i in lista:
+#listasuma.append(1.vari+ 1.var3+ 1.vars)
+#return listasuma
